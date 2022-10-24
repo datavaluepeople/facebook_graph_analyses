88faf72e2e63881d5edccc1c289f8f9bcfbc1e65
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2022-10-24 16:18:24 +0200

    Add session to FacebookGraphConfig

--- facebook_graph_analyses/facebook_graph/configuration.py
+++ facebook_graph_analyses/facebook_graph/configuration.py
@@ -2,6 +2,8 @@
 import re
 from dataclasses import dataclass
 
+import requests
+
 
 DEFAULT_API_VERSION = "v15.0"
 
@@ -12,11 +14,13 @@ class FacebookGraphConfig:
 
     api_version: str
     access_token: str
+    session: requests.Session
 
 
 def create_config(
     access_token: str,
     api_version: str = DEFAULT_API_VERSION,
+    session: requests.Session = requests.Session(),
 ) -> FacebookGraphConfig:
     """Create a FacebookGraphConfig."""
     api_pattern = re.compile(r"^v\d+\.\d+$")
@@ -24,7 +28,10 @@ def create_config(
         raise ValueError(f"API version is not correctly {api_version}")
     if not access_token:
         raise ValueError(f"Access Token must be given: {access_token}")
+    if not isinstance(session, requests.Session):
+        raise ValueError(
+            f"Session must be a requests.session: {type(session)} was given"
+        )
     return FacebookGraphConfig(
-        api_version=api_version,
-        access_token=access_token,
+        api_version=api_version, access_token=access_token, session=session
     )
--- tests/facebook_graph/test_configuration.py
+++ tests/facebook_graph/test_configuration.py
@@ -1,5 +1,6 @@
 """Test facebook_graph configuration."""
 import pytest
+import requests
 
 from facebook_graph_analyses.facebook_graph import configuration
 
@@ -11,6 +12,7 @@ def test_create_config_default():
     assert isinstance(facebook_graph_cfg, configuration.FacebookGraphConfig)
     assert facebook_graph_cfg.access_token == access_token
     assert facebook_graph_cfg.api_version == configuration.DEFAULT_API_VERSION
+    assert isinstance(facebook_graph_cfg.session, requests.Session)
 
 
 def test_create_config_errors_access_token():
@@ -21,17 +23,30 @@ def test_create_config_errors_access_token():
         assert "Access Token" in str(err)
 
 
-def test_create_config_with_api_version():
-    """Test create config for a given api version."""
+def test_create_config_errors_session():
+    """Test create config for default."""
+    access_token = "access_token"
+    session = "session"
+    with pytest.raises(ValueError) as err:
+        _ = configuration.create_config(
+            access_token=access_token, session=session  # type: ignore
+        )
+        assert "Session" in str(err)
+        assert "String was given" in str(err)
+
+
+def test_create_config_with_params():
+    """Test create config for a given api version and session."""
     api_version = "v14.0"
     access_token = "access_token"
+    session = requests.Session()
     facebook_graph_cfg = configuration.create_config(
-        access_token=access_token,
-        api_version=api_version,
+        access_token=access_token, api_version=api_version, session=session
     )
     assert isinstance(facebook_graph_cfg, configuration.FacebookGraphConfig)
     assert facebook_graph_cfg.access_token == access_token
     assert facebook_graph_cfg.api_version == api_version
+    assert facebook_graph_cfg.session == session
 
 
 @pytest.mark.parametrize("invalid_api_version", ["1", "v1", "14.0", "f1"])
